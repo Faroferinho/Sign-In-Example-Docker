eb598ea9593dea8515759f8dd70c13781163a8b5
Faroferinho <conrado.perini.fracacio@gmail.com>
2025-04-23 11:50:12 -0300

    updating the properties of the API

--- MongoAPI/src/main/java/org/example/Main.java
+++ MongoAPI/src/main/java/org/example/Main.java
@@ -2,8 +2,11 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.POST})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
